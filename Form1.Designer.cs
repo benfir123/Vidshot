93fb857ea97af0667fffcf94af303367603c6e71
Ben Chanapai <ben_chanapai04@hotmail.com>
2021-05-18 20:48:21 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Vidshot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(136, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(113, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Helvetica", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(85, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sign in with Google";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Helvetica", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(85, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sign in with Facebook";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Helvetica", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(85, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 29);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Welcome to Vidshot";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(396, 511);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sign in with Google";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Vidshot
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+
+            InitializeComponent();
+            this.Opacity = .5D;
+
+        }
+
+        bool mouseDown = false;
+        bool PointExists = false;
+        Point mouseDownPoint = Point.Empty;
+        Point mousePoint = Point.Empty;
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            mouseDown = true;
+            mousePoint = mouseDownPoint = e.Location;
+            PointExists = false;
+            
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            mouseDown = false;
+            PointExists = true;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+           if(PointExists == false)
+            {
+                base.OnMouseMove(e);
+                mousePoint = e.Location;
+                Invalidate();
+            }
+            
+            
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Region r = new();
+            Rectangle window;
+            
+
+
+            if (mouseDown)
+            {
+               this.DoubleBuffered = true;
+               this.BackColor = Color.White;
+               this.TransparencyKey = Color.White;
+               toolStrip1.Visible = false;
+
+                window = new(
+                    Math.Min(mouseDownPoint.X, mousePoint.X),
+                    Math.Min(mouseDownPoint.Y, mousePoint.Y),
+                    Math.Abs(mouseDownPoint.X - mousePoint.X),
+                    Math.Abs(mouseDownPoint.Y - mousePoint.Y));
+                    r.Xor(window);
+
+                e.Graphics.FillRegion(Brushes.Black, r);
+
+                Console.WriteLine("Painted: " + window);
+
+                ControlPaint.DrawReversibleFrame(window, Color.FromArgb(80, 120, 120, 120), FrameStyle.Dashed);
+            }
+
+            if (PointExists)
+            {
+
+                window = new(
+                          Math.Min(mouseDownPoint.X, mousePoint.X),
+                          Math.Min(mouseDownPoint.Y, mousePoint.Y),
+                          Math.Abs(mouseDownPoint.X - mousePoint.X),
+                          Math.Abs(mouseDownPoint.Y - mousePoint.Y));
+                r.Xor(window);
+                e.Graphics.FillRegion(Brushes.Black, r);
+
+                ControlPaint.DrawReversibleFrame(window, Color.FromArgb(80, 120, 120, 120), FrameStyle.Dashed);
+
+                toolStrip1.Location = new Point(window.X + window.Width - 111, window.Y + window.Height);
+                
+                
+
+
+
+
+
+
+
+
+
+
+            }    
+        
+        
+
+
+        }
+
+        private void Form2_MouseUp(object sender, MouseEventArgs e)
+        {
+            toolStrip1.Visible = true;
+        }
+    }
+}
